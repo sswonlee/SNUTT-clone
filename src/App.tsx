be7e6e789383350ed09153ac536c4381196a9cb7
anvil1028 <anvil1028@snu.ac.kr>
2024-10-15 22:23:10 +0900

    Login Screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,24 @@ import 'pretendard/dist/web/static/pretendard.css';
 
 import AppBar from './components/AppBar';
 import Home from './components/Home';
+import { useState } from 'react';
+import Nickname from './components/Nickname';
+import Login from './components/Login';
+import type { Screen } from './types';
 
 export const App = () => {
+  const [screen, setScreen] = useState<Screen>('home');
+
   return (
-    <div className="w-[375px] h-[800px] flex flex-col pb-[90px] rounded-md outline outline-1">
+    <div className="w-[375px] h-[800px] flex flex-col rounded-md outline outline-1">
       <AppBar />
-      <Home />
+      {screen === 'home' ? (
+        <Home setScreen={setScreen} />
+      ) : screen === 'login' ? (
+        <Login setScreen={setScreen} />
+      ) : (
+        <Nickname />
+      )}
     </div>
   );
 };
