a855c671142c5c30029ca369e370fc1e4580e814
baek minwoo <bmw06004@gmail.com>
2024-10-16 16:21:33 +0900

    🔥안 쓰는 type 정리

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,4 @@
 export type Screen = 'home' | 'login' | 'nickname';
-export type Response = {
-  user_id: string;
-  token: string;
-  message: string;
-};
-export interface LogInType {
-  user_id: string;
-  token: string;
-  message: string;
-}
-
-export interface LogInRes {
-  id: string;
-  password: string;
-}
 
 export type UserInfo = {
   id: string;
