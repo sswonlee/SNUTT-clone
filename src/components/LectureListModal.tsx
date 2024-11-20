96ab7e96f411ef12f18254dcf93847727734da5f
baek minwoo <bmw06004@gmail.com>
2024-11-20 15:15:54 +0900

    ✨list modal tailwind 정리

--- src/components/LectureListModal.tsx
+++ src/components/LectureListModal.tsx
@@ -1,10 +1,11 @@
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import type { Dispatch, SetStateAction } from 'react';
 import { useState } from 'react';
 
 import Bookmark from '../assets/LectureList/Bookmark.svg';
 import Location from '../assets/LectureList/Location.svg';
 import Time from '../assets/LectureList/Time.svg';
-import Back from '../assets/Left.png';
 import type { Lecture } from '../types';
 import NewLectureModal from './NewlectureModal';
 
@@ -34,72 +35,72 @@ const LectureListModal = ({
       ) : (
         <div className="flex w-full h-full flex-col justify-start">
           <div className="flex h-10 w-full items-center px-4 border-b border-solid border-neutral-200 box-border">
-            <div className="flex w-full items-center">
+            <div className="flex w-full items-center justify-between">
               <button
                 onClick={() => {
                   setListModal(false);
                 }}
-                className="w-4 h-4"
+                className="flex items-center hover:text-zinc-300 duration-200"
               >
-                <img src={Back} />
+                <FontAwesomeIcon icon={faChevronLeft} className="mr-1" />
+                <p className="font-bold">{title}</p>
               </button>
-              <div className="w-3"></div>
-              <p className="text-[17px] font-bold">{title}</p>
               <button
                 className="ml-auto items-center pb-[5px]"
                 onClick={() => {
                   setModal(true);
                 }}
               >
-                <p className="text-[30px] font-bold">+</p>
+                <p className="text-3xl hover:text-zinc-300 duration-200">+</p>
               </button>
             </div>
           </div>
-          <div className="flex flex-col overflow-auto overscroll-contain">
+          <div className="flex flex-col overflow-y-auto overscroll-contain">
             {list.map((lecture, i) => (
               <div
                 onClick={() => {
                   setLecture(lecture);
                 }}
                 key={i}
-                className="flex flex-col pl-4"
+                className="flex flex-col p-4 space-y-2 border-b-[1px] border-zinc-200"
               >
-                <div className="flex">
+                <div className="flex justify-between">
                   <div className="font-bold">{lecture.course_title}</div>
-                  <div className="ml-auto">
+                  <div>
                     {lecture.instructor} / {lecture.credit}학점
                   </div>
                 </div>
-                <div className="flex">
-                  <img src={Bookmark} className="mt-[2px] w-4 h-4" />
-                  <div className="ml-2 text-[13px]">
+                <div className="flex items-center space-x-2">
+                  <img src={Bookmark} className="w-4" />
+                  <div className="text-sm">
                     {lecture.category !== '' ? `${lecture.category}, ` : ''}
                     {lecture.department}, {lecture.academic_year}
                   </div>
                 </div>
-                <div className="flex">
-                  <img src={Time} className="mt-[2px] w-4 h-4" />
-                  <div className="ml-2 text-[13px] flex flex-row">
+                <div className="flex w-full items-start space-x-2">
+                  <img src={Time} className="mt-[2px] w-4" />
+                  <div className="text-sm flex flex-wrap justify-start">
                     {lecture.class_time_json.map((time, j) => {
-                      if (j === lecture.class_time_json.length - 1)
+                      if (j === lecture.class_time_json.length - 1) {
                         return (
                           <div key={j}>
                             {days[time.day]}({time.start_time}~{time.end_time})
                           </div>
                         );
-                      else
+                      } else {
                         return (
-                          <div key={j}>
+                          <div key={j} className="flex w-fit">
                             {days[time.day]}({time.start_time}~{time.end_time}
                             ),&nbsp;
                           </div>
                         );
+                      }
                     })}
                   </div>
                 </div>
-                <div className="flex">
-                  <img src={Location} className="mt-[2px] w-4 h-4" />
-                  <div className="ml-2 flex flex-row text-[13px]">
+                <div className="flex w-full items-start space-x-2">
+                  <img src={Location} className="w-4" />
+                  <div className="flex text-sm">
                     {lecturePlace(lecture).map((place, j) => {
                       if (j === lecturePlace(lecture).length - 1)
                         return <div key={j}>{place}</div>;
