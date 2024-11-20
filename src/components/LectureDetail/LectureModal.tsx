e8f6aa89a03379369c2e66f9ef859402b6d253ff
baek minwoo <bmw06004@gmail.com>
2024-11-20 21:27:49 +0900

    🔥html 되돌리고 lecture list에서 내부 div에 스크롤 적용으로 해결

--- src/components/LectureDetail/LectureModal.tsx
+++ src/components/LectureDetail/LectureModal.tsx
@@ -49,7 +49,7 @@ const LectureModal = () => {
 
       return (
         <div className="h-full overflow-y-auto">
-          <div className="flex flex-col bg-neutral-200 w-full h-full space-y-3">
+          <div className="flex flex-col bg-neutral-200 w-full h-full">
             <div className="h-fit py-2 px-4 bg-white">
               <Link
                 to={`/timetable/${tableID}`}
@@ -59,48 +59,50 @@ const LectureModal = () => {
                 <p className="font-bold">강의 상세 보기</p>
               </Link>
             </div>
-            <MenuGroup
-              menuArray={[
-                { title: '강의명', info: lecture.course_title },
-                { title: '교수', info: lecture.instructor },
-                {
-                  title: '색상',
-                  info: (
-                    <div className="flex border border-solid border-black">
-                      <div className="w-5 h-5 bg-white"></div>
-                      <div
-                        className="w-5 h-5"
-                        style={{
-                          backgroundColor: color[lecture.colorIndex - 1],
-                        }}
-                      ></div>
-                    </div>
-                  ),
-                },
-              ]}
-            ></MenuGroup>
-            <MenuGroup
-              menuArray={[
-                { title: '학과', info: lecture.department },
-                { title: '학년', info: lecture.academic_year },
-                { title: '학점', info: lecture.credit },
-                { title: '분류', info: lecture.category },
-                { title: '구분', info: lecture.classification },
-                { title: '강좌번호', info: lecture.course_number },
-                { title: '분반번호', info: lecture.lecture_number },
-                { title: '정원', info: lecture.quota },
-                { title: '비고', info: lecture.remark },
-              ]}
-            ></MenuGroup>
-            <MenuGroup
-              menuArray={[
-                { title: '시간 및 장소', info: ' ' },
-                ...timeAndPlaceArr,
-              ]}
-            ></MenuGroup>
-            <button className="flex w-full h-10 p-2 bg-white text-sm text-red-500 justify-center items-center hover:bg-red-200 duration-200">
-              삭제
-            </button>
+            <div className="overflow-y-auto space-y-3">
+              <MenuGroup
+                menuArray={[
+                  { title: '강의명', info: lecture.course_title },
+                  { title: '교수', info: lecture.instructor },
+                  {
+                    title: '색상',
+                    info: (
+                      <div className="flex border border-solid border-black">
+                        <div className="w-5 h-5 bg-white"></div>
+                        <div
+                          className="w-5 h-5"
+                          style={{
+                            backgroundColor: color[lecture.colorIndex - 1],
+                          }}
+                        ></div>
+                      </div>
+                    ),
+                  },
+                ]}
+              ></MenuGroup>
+              <MenuGroup
+                menuArray={[
+                  { title: '학과', info: lecture.department },
+                  { title: '학년', info: lecture.academic_year },
+                  { title: '학점', info: lecture.credit },
+                  { title: '분류', info: lecture.category },
+                  { title: '구분', info: lecture.classification },
+                  { title: '강좌번호', info: lecture.course_number },
+                  { title: '분반번호', info: lecture.lecture_number },
+                  { title: '정원', info: lecture.quota },
+                  { title: '비고', info: lecture.remark },
+                ]}
+              ></MenuGroup>
+              <MenuGroup
+                menuArray={[
+                  { title: '시간 및 장소', info: ' ' },
+                  ...timeAndPlaceArr,
+                ]}
+              ></MenuGroup>
+              <button className="flex w-full h-10 p-2 bg-white text-sm text-red-500 justify-center items-center hover:bg-red-200 duration-200">
+                삭제
+              </button>
+            </div>
           </div>
         </div>
       );
